079c745028918da8b44a33234e649eb01356e2bc
Vamshi Vignesh <bagulavamshivignesh25@gmail.com>
2023-01-09 01:48:53 +0530

    Added tests taking reference from MyTodoApp

---
+++ __tests__/elections.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const request = require("supertest");
+const cheerio = require("cheerio");
+//const csrf = require("tiny-csrf")
+const db = require("../models/index");
+const app = require("../app");
+let server, agent;
+function extractCsrfToken(res) {
+  var $ = cheerio.load(res.text);
+  return $("[name=_csrf]").val();
+}
+describe("Online Voting Application", function () {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+    server = app.listen(3000, () => {});
+    agent = request.agent(server);
+  });
+
+  afterAll(async () => {
+    try {
+      await db.sequelize.close();
+      await server.close();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  test("Sign up test for Admin", async () => {
+    let res = await agent.get("/signup");
+    const csrfToken = extractCsrfToken(res);
+    res = await agent.post("/Admin").send({
+      username: "User A",
+      email: "usera@test.com",
+      password: "12345678",
+      _csrf: csrfToken,
+    });
+    expect(res.statusCode).toBe(302);
+  });
+  test("Sign Out test for Admin", async () => {
+    let res = await agent.get("/");
+    const csrfToken = extractCsrfToken(res);
+    res = await agent.get("/signout").send({
+      _csrf: csrfToken,
+    });
+    expect(res.statusCode).toBe(302);
+  });
+});
